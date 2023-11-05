f199328a76e5e8214f57430b1cbb8bb24ec8f7ec
VitMach <machihin_vitalii@mail.ru>
2023-11-06 01:22:33 +0300

    add filters

--- src/components/Controls.jsx
+++ src/components/Controls.jsx
@@ -1,41 +1,51 @@
+import { useSelector, useDispatch } from 'react-redux';
+
 import styled from 'styled-components';
 
 import { Search } from './Search';
 import { CustomSelect } from './CustomSelect';
+import { setRegion } from '../store/controls/controls-actions';
+import { selectRegion } from '../store/controls/controls-selector';
 
 const optionsMap = {
-  'Africa': { value: 'Africa', label: 'Africa' },
-  'America': { value: 'America', label: 'America' },
-  'Asia': { value: 'Asia', label: 'Asia' },
-  'Europe': { value: 'Europe', label: 'Europe' },
-  'Oceania': { value: 'Oceania', label: 'Oceania' },
-}
+	Africa: { value: 'Africa', label: 'Africa' },
+	America: { value: 'America', label: 'America' },
+	Asia: { value: 'Asia', label: 'Asia' },
+	Europe: { value: 'Europe', label: 'Europe' },
+	Oceania: { value: 'Oceania', label: 'Oceania' },
+};
 const options = Object.values(optionsMap);
 
 const Wrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
+	display: flex;
+	flex-direction: column;
+	align-items: flex-start;
 
-  @media (min-width: 767px) {
-    flex-direction: row;
-    justify-content: space-between;
-    align-items: center;
-  }
+	@media (min-width: 767px) {
+		flex-direction: row;
+		justify-content: space-between;
+		align-items: center;
+	}
 `;
 
 export const Controls = () => {
-  return (
-    <Wrapper>
-      <Search />
-      <CustomSelect
-        options={options}
-        placeholder="Filter by Region"
-        isClearable
-        isSearchable={false}
-        value={''}
-        onChange={() => {}}
-      />
-    </Wrapper>
-  );
+	const dispatch = useDispatch();
+	const region = useSelector(selectRegion);
+
+	const handleSelectRegion = (region) => {
+		dispatch(setRegion(region?.value || ''));
+	};
+	return (
+		<Wrapper>
+			<Search />
+			<CustomSelect
+				options={options}
+				placeholder='Filter by Region'
+				isClearable
+				isSearchable={false}
+				value={optionsMap[region]}
+				onChange={handleSelectRegion}
+			/>
+		</Wrapper>
+	);
 };
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,48 +1,74 @@
 import { useNavigate } from 'react-router-dom';
 
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+
+import { loadCountries } from '../store/countries/countries-actions';
+import {
+	selectCountriesInfo,
+	selectVisibleCountries,
+} from '../store/countries/countries-selector';
+
 import { List } from '../components/List';
 import { Card } from '../components/Card';
 import { Controls } from '../components/Controls';
+import { selectControls } from '../store/controls/controls-selector';
 
 export const HomePage = () => {
-  const navigate = useNavigate();
+	const navigate = useNavigate();
+	const dispatch = useDispatch();
 
-  const countries = [];
+	const { search, region } = useSelector(selectControls);
+	const countries = useSelector((state) =>
+		selectVisibleCountries(state, { search, region })
+	);
+	const { status, qty, err } = useSelector(selectCountriesInfo);
 
-  return (
-    <>
-      <Controls />
+	useEffect(() => {
+		if (!qty) {
+			dispatch(loadCountries());
+		}
+	}, [qty, dispatch]);
 
-      <List>
-            {countries.map((c) => {
-              const countryInfo = {
-                img: c.flags.png,
-                name: c.name,
-                info: [
-                  {
-                    title: 'Population',
-                    description: c.population.toLocaleString(),
-                  },
-                  {
-                    title: 'Region',
-                    description: c.region,
-                  },
-                  {
-                    title: 'Capital',
-                    description: c.capital,
-                  },
-                ],
-              };
+	return (
+		<>
+			<Controls />
 
-              return (
-                <Card
-                  key={c.name}
-                  onClick={() => navigate(`/country/${c.name}`)}
-                  {...countryInfo}
-                />
-              );
-            })}
-          </List>
-    </>
-  );
+			{err && <h2>Ошибка при загрузке данных</h2>}
+			{status === 'loading' && <h2>Загрузка...</h2>}
+
+			{status === 'fulfilled' && (
+				<List>
+					{countries.map((c) => {
+						const countryInfo = {
+							img: c.flags.png,
+							name: c.name,
+							info: [
+								{
+									title: 'Population',
+									description: c.population.toLocaleString(),
+								},
+								{
+									title: 'Region',
+									description: c.region,
+								},
+								{
+									title: 'Capital',
+									description: c.capital,
+								},
+							],
+						};
+
+						return (
+							<Card
+								key={c.name}
+								onClick={() => navigate(`/country/${c.name}`)}
+								{...countryInfo}
+							/>
+						);
+					})}
+				</List>
+			)}
+		</>
+	);
 };
